5d5bd06faee10912e94c48437d6253b4ed89e092
Smedha245-unclejoy <priti7608@gmail.com>
2018-05-24 17:02:56 +0530

    gda

--- PlayYourWay/settings.py
+++ PlayYourWay/settings.py
@@ -185,4 +185,3 @@ ENV_PATH = os.path.abspath(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')
 MEDIA_URL = '/media/'
 django_heroku.settings(locals())
-GDAL_LIBRARY_PATH = config('GDAL_LIBRARY_PATH')
