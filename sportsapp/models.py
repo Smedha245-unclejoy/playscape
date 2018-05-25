19632bea9192bb16e92213b365d7d72eede0a103
Smedha245-unclejoy <priti7608@gmail.com>
2018-05-25 12:43:15 +0530

    geo

--- sportsapp/models.py
+++ sportsapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.fields import DateField
 from django.contrib.auth.models import User
-from django.contrib.gis.db import models
+from django.contrib.gis.db import models as gis_models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -16,6 +16,6 @@ class Profile(models.Model):
                         )
     gender = models.CharField(choices = gender_choices,max_length=1)
     dob = models.DateField()
-    last_location = models.PointField(max_length=40, null=True)
+    last_location = gis_models.PointField(max_length=40, null=True)
     prefered_radius = models.IntegerField(default=5, help_text="in kilometers")
-    objects = models.GeoManager()
+    objects = gis_models.GeoManager()
