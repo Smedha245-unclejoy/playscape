1eb4748f400627ab822aaf7f8a39e70437f03021
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-02 18:43:43 +0530

    p

--- sportsapp/models.py
+++ sportsapp/models.py
@@ -16,6 +16,6 @@ class Profile(models.Model):
                         ('O','Others'))
     user_gender = models.CharField(choices = gender_choices,max_length=1,null=True)
     #dob = models.DateField()
-    last_location = models.PointField(max_length=40, null=True)
+    last_location = models.PointField(max_length=40, null=True,spatial_index=True, geography=True)
     prefered_radius = models.IntegerField(default=5, help_text="in kilometers")
     objects = GeoManager()
