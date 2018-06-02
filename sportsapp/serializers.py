727036346a96a1019529cdde9ccecf9f23424ad1
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-03 00:08:27 +0530

    and

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -31,7 +31,7 @@ class UserSerializer(GeoFeatureModelSerializer):
     user_gender = serializers.ChoiceField(source='profile.user_gender',choices=gender_choices)
     #dob = serializers.DateField(source='profile.dob')  # date in the format 1995-12-17:yyyy-mm-dd
     #posts = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='post-detail')
-    last_location = geo_serializers.GeometrySerializerMethodField()
+    last_location = GeometrySerializerMethodField(required=False,spurce='profile.last_location')
     prefered_radius = serializers.IntegerField(source='profile.prefered_radius',default=5)
 
     class Meta:
