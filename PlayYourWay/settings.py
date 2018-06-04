97006dea281573b94cfcd9032cd39f5b54c41ccd
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-04 20:15:37 +0530

    and

--- PlayYourWay/settings.py
+++ PlayYourWay/settings.py
@@ -119,10 +119,11 @@ DATABASES = {
 #db_from_env = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 #DATABASES['default'].update(db_from_env)
 DATABASES['default']['ENGINE']='django.contrib.gis.db.backends.postgis'
+print(DATABASES['default'])
 if os.getenv('DYNO'):
     DATABASES['default'] =  dj_database_url.parse(os.getenv('DATABASE_URL'),'django.contrib.gis.db.backends.postgis')
     print(DATABASES['default'])
-print(DATABASES['default'])
+
 #DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 #DATABASES['default']['NAME'] = 'postgres'
 #DATABASES['default']['USER'] = 'postgres'
