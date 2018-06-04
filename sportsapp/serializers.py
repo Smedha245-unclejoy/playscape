ea894edf1c85f4b1e35365e717f761734a7319ba
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-04 12:23:42 +0530

    and

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -33,12 +33,14 @@ class UserSerializer(GeoFeatureModelSerializer):
     user_gender = serializers.ChoiceField(source='profile.user_gender',choices=gender_choices)
     #dob = serializers.DateField(source='profile.dob')  # date in the format 1995-12-17:yyyy-mm-dd
     #posts = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='post-detail')
-    last_location = Point(source='profile.last_location')
+    last_location = geo_serializers.GeometryField(source='profile.last_location')
     prefered_radius = serializers.IntegerField(source='profile.prefered_radius',default=5)
 
-    def get_last_location(self, instance):
-        print(instance)
-        return Point(instance.last_location.lat, instance.last_location.lon)
+    def to_representation(self, instance):
+        ret = super(UserSerializer, self).to_representation(instance)
+        pnt = ret['last_location']
+        ret['last_location'] = {'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
+        return ret
 
     class Meta:
         model = User
