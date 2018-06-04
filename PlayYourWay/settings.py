7944065f9acd2d3af7a0757e5613073cbbf8ec49
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-04 19:45:31 +0530

    and

--- PlayYourWay/settings.py
+++ PlayYourWay/settings.py
@@ -120,11 +120,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME' : 'postgres',
-        'USER' : 'postgres'
     }
 }
 db_from_env = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'].update('django.contrib.gis.db.backends.postgis')
 #DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 #DATABASES['default']['NAME'] = 'postgres'
 #DATABASES['default']['USER'] = 'postgres'
