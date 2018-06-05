e6b61bd6ee0ea646956c33d15cee8aae5b350cd0
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-05 15:35:15 +0530

    mp

--- sportsapp/views.py
+++ sportsapp/views.py
@@ -110,7 +110,7 @@ class AuthInfoUpdateView(generics.UpdateAPIView):
 
     def patch(self, request, *args, **kwargs):
         instance=get_object_or_404(User,email=request.data['email'])
-        serializer = UserSerializer(instance=instance,data={'last_location'=request.data['last_location'],'prefered_radius'=request.data['prefered_radius'],
+        serializer = UserSerializer(data={'last_location'=request.data['last_location'],'prefered_radius'=request.data['prefered_radius'],
                                          'user_gender'=request.data['user_gender'],'first_name'=request.data['first_name'],'email'=request.data['email']
                                          'password'=request.data['password']})
         if serializer.is_valid():
