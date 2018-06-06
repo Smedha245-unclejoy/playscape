a1cfab758230e2e23bb92a50a6f6b62ce183c52e
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-06 19:09:09 +0530

    url

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -49,8 +49,8 @@ class UserSerializer(serializers.ModelSerializer):
     def get_last_location(self, instance):
         ret = instance
         pnt = fromstr(instance.profile.last_location)
-        instance.profile.last_location={'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
-        return instance
+        pnt={'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
+        return pnt
 
 
 #By overriding create and update any put or post delete will be in sync with the profile table
