88f8b5d461cd3748c8341dd1758a752452b40081
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-06 17:40:00 +0530

    url

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -37,7 +37,7 @@ class UserSerializer(serializers.ModelSerializer):
     user_gender = serializers.ChoiceField(source='profile.user_gender',choices=gender_choices)
     #dob = serializers.DateField(source='profile.dob')  # date in the format 1995-12-17:yyyy-mm-dd
     #posts = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='post-detail')
-    last_location = serializers.SerializerMethodField(required=False,source='profile.last_location')
+    last_location = serializers.SerializerMethodField(required=False)
     prefered_radius = serializers.IntegerField(source='profile.prefered_radius',default=5)
 
 
