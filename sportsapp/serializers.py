5770e3e7e781d9099f7a401bdfd4a08acfeefc38
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-06 16:26:42 +0530

    url

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -47,10 +47,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'first_name', 'email', 'password','last_location','user_gender','prefered_radius')
 
     def get_last_location(self, instance):
-        ret = instance
+        ret = instance.data
         pnt = fromstr(ret['profile']['last_location'])
-        ret.profile.last_location = ret['last_location']
-        ret['last_location'] = {'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
+        ret['profile']['last_location'] = {'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
         return ret
 
 
