8d8cd965d03cadadcb3ef85ea022ea74a6667632
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-06 13:17:58 +0530

    url

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -37,7 +37,7 @@ class UserSerializer(serializers.ModelSerializer):
     user_gender = serializers.ChoiceField(source='profile.user_gender',choices=gender_choices)
     #dob = serializers.DateField(source='profile.dob')  # date in the format 1995-12-17:yyyy-mm-dd
     #posts = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='post-detail')
-    last_location = serializers.SerializerMethodField(required=False,source='profile.last_location')
+    last_location = serializers.SerializerMethodField(required=False,source='profile.last_location',method_name=to_representation)
     prefered_radius = serializers.IntegerField(source='profile.prefered_radius',default=5)
 
 
@@ -47,10 +47,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'first_name', 'email', 'password','last_location','user_gender','prefered_radius')
 
     def to_representation(self, instance):
-        ret = instance
-        location=ret.profile
-        pnt = fromstr(ret.profile.last_location)
-        ret.profile['last_location'] = {'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
+        ret = instance.profile.last_location
+        pnt = fromstr(ret)
+        ret = {'longitude': pnt.coords[0], 'latitude': pnt.coords[1]}
         return ret
 
 
