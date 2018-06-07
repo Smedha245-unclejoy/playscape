19577447ae2b876c51b10d3293f102196d4f8998
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-07 16:02:34 +0530

    km

--- sportsapp/serializers.py
+++ sportsapp/serializers.py
@@ -35,8 +35,6 @@ class UserSerializer(serializers.ModelSerializer):
                         ('O','Others')
                         )
     user_gender = serializers.ChoiceField(source='profile.user_gender',choices=gender_choices)
-    latitude = serializers.FloatField(source='profile.latitude')
-    longitude = serializers.FloatField(source='profile.longitude')
     #dob = serializers.DateField(source='profile.dob')  # date in the format 1995-12-17:yyyy-mm-dd
     #posts = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='post-detail')
     last_location = serializers.SerializerMethodField(required=False,source='profile.last_location',method_name='to_representation')
@@ -46,7 +44,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'email', 'password','last_location','latitude','longitude','user_gender','prefered_radius')
+        fields = ('id', 'first_name', 'email', 'password','last_location','user_gender','prefered_radius')
 
     def to_representation(self, instance):
         ret = instance
