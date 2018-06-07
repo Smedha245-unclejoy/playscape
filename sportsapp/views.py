236acb702e5016725e7a06457136f34281135fb2
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-07 20:27:13 +0530

    long

--- sportsapp/views.py
+++ sportsapp/views.py
@@ -114,7 +114,7 @@ class AuthInfoUpdateView(generics.UpdateAPIView):
             serializer = UserSerializer(instance=instance,data=request.data)
             if serializer.is_valid():
                 serializer.update(instance=instance,validated_data=serializer.data)
-                return Response(serializer.data)
+                return Response(JsonResponse(serializer.data),safe=False)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
