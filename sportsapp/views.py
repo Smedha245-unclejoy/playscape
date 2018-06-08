32489b4d167ee52bd4733df0304bbe29f0f64cd8
Smedha245-unclejoy <priti7608@gmail.com>
2018-06-08 20:53:57 +0530

    mnm

--- sportsapp/views.py
+++ sportsapp/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.response import JsonResponse
 from rest_framework import generics
 from rest_framework.parsers import JSONParser
 from django.contrib.auth import authenticate
