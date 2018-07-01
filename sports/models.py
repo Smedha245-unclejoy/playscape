16301de0990862360b8edfcce311ff4fd0e58301
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-01 16:42:04 +0530

    pta

--- sports/models.py
+++ sports/models.py
@@ -10,7 +10,7 @@ class Sport(models.Model):
      team_choices = (('Single Team','SingleTeam'),
                          ('Multiple Team','MultipleTeam'),
                          )
-     team = models.ChoiceField(choices=team_choices,max_length=15)
+     team = models.CharField(choices=team_choices,max_length=15)
 
      class Meta:
         ordering = ('sport_name',)
@@ -21,5 +21,5 @@ class Sport(models.Model):
 
 class SportFollower(models.Model):
     sport = models.ForeignKey(Sport,on_delete=models.CASCADE)
-    sport_name = models.ChoiceField(choices = [ (str(o), str(o)) for o in Sport.objects.filter(pk=self.sport)])
+    sport_name = models.CharField(choices = [ (str(o), str(o)) for o in Sport.objects.filter(pk=self.sport)],max_length=20)
     follower = models.ForeignKey(User,on_delete=models.CASCADE)
