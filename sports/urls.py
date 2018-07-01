52ae15e1b63dd2b550478df11eb71617f5a286cd
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-01 19:44:29 +0530

    hui

--- sports/urls.py
+++ sports/urls.py
@@ -1,1 +1,12 @@
+from .views import SportView,SportFollowerView,SportsFollowedByUser
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    url(r'follow/$', SportFollowerView.as_view(), name='follow_sport'),
+    url(r'followedlist/(?P<user_id>\d+)/$',SportsFollowedByUser.as_view() , name='sports_followed'),
+    url(r'^$', SportView.as_view(),name='create_sport')
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
