b3d0bab937c6cdcb52c2a55731610bb9a0f72f8c
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-04 18:39:53 +0530

    pulp

--- sports/views.py
+++ sports/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from sports.models import Sport,SportFollower
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.decorators import parser_classes
