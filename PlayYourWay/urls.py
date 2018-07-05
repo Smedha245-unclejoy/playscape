68e07f39cfdaff43f770605a6b6fda987b696037
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-05 17:35:02 +0530

    ulp

--- PlayYourWay/urls.py
+++ PlayYourWay/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     url(r'^sport/', include('sports.urls'))
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
