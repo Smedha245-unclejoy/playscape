18bc57e0e757d5ce48dc62b4a1d6b0f443c8131e
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-19 12:25:49 +0530

    ol

--- sportsapp/views.py
+++ sportsapp/views.py
@@ -152,5 +152,5 @@ class GetAllUsers(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
     def get_queryset(self):
-        queryset = User.objects.all()
+        queryset = User.objects.filter(is_superuser=false)
         return queryset
