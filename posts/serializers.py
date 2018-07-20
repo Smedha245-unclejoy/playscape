6115612d862089f648c1e7e68d5fbf9f1f8ebe52
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-20 19:10:06 +0530

    manage

--- posts/serializers.py
+++ posts/serializers.py
@@ -21,5 +21,5 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
         task = Post.objects.update_or_create(body = validated_data.get('body', 'no-title'),is_active = validated_data.get('is_active', True),
                         user_id=self.context.get('view').request.user)
         for image_data in images_data:
-            PostImage.objects.update_or_create(post=task, image=image_data)
+            PostImage.objects.update_or_create(post=task.id, image=image_data)
         return task
