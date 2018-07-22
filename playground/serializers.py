15be51e41e3e435e36e8108b52ceae2ee28ebfe5
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-22 19:54:03 +0530

    ifs

--- playground/serializers.py
+++ playground/serializers.py
@@ -8,7 +8,7 @@ from django.contrib.gis.geos import fromstr
 
 
 class PlaygroundSerializer(serializers.ModelSerializer):
-       picture = Base64ImageField(source='playground.picture',required=False)
+       picture = Base64ImageField(required=False,use_url=True,max_length=None)
        location = serializers.SerializerMethodField(required=False,source='playground.location',read_only=True)
 
        class Meta:
