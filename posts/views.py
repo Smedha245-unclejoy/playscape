37c73b41d8d3809167dea2e68f6ed1fa9dcd36fc
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-25 13:04:08 +0530

    pfeed

--- posts/views.py
+++ posts/views.py
@@ -67,6 +67,6 @@ class PostFeed(generics.ListAPIView):
     serializer = PostSerializer
     def get_queryset(self):
         author = Friendship.objects.filter(Q(friend_id=self.request.user.id)|Q(creator_id=self.request.user.id))
-        friend_id = author.friend_id.all()|author.creator_id.all()
+        friend_id = author.friend.all()|author.creator.all()
         queryset = Post.objects.filter(is_active=True,user_id=friend_id)
         return queryset
