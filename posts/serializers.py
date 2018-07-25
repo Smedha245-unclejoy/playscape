522e89a8a5df727041b77cceda98ffa36dfabc5d
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-25 14:40:57 +0530

    pfeed

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,9 +8,9 @@ class PostImageSerializer(serializers.ModelSerializer):
         fields = ('image','post')
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-    user_id = serializers.ReadOnlyField(source='user.id')
+    #user_id = serializers.ReadOnlyField(source='user.id')
     images = PostImageSerializer(source='postimage_set', many=True, read_only=True)
-    body = serializers.CharField(required=True)
+    #body = serializers.CharField(required=True)
     class Meta:
         model = Post
         fields = ('id','user_id','body','created_at','is_active','images')
@@ -19,7 +19,7 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
         #print("Inside serializers")
         images_data = self.context.get('view').request.FILES.getlist('file')
         task = Post.objects.create(body = validated_data.get('body', 'no-title'),is_active = validated_data.get('is_active', True),
-                        user_id=self.context.get('view').request.user)
+                        user_id=self.context.get('view').request.user.id)
         for image_data in images_data:
             PostImage.objects.create(post=task, image=image_data)
         return task
