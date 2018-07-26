58e004a7145804102149ac2cda71b13491b65de8
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-26 13:47:33 +0530

    post edit

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('User',on_delete=models.CASCADE)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     body = models.TextField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
--- posts/serializers.py
+++ posts/serializers.py
@@ -8,7 +8,6 @@ class PostImageSerializer(serializers.ModelSerializer):
         fields = ('image','post')
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='user.id')
     images = PostImageSerializer(source='postimage_set', many=True, read_only=True)
     #body = serializers.CharField(required=True)
     class Meta:
@@ -19,7 +18,7 @@ class PostSerializer(serializers.ModelSerializer):
         #print("Inside serializers")
         images_data = self.context.get('view').request.FILES.getlist('file')
         task = Post.objects.create(body = validated_data.get('body', 'no-title'),is_active = validated_data.get('is_active', True),
-                        author=self.context.get('view').request.user)
+                        author=self.context.get('view').request.user.pk)
         for image_data in images_data:
             PostImage.objects.create(post=task, image=image_data)
         return task
