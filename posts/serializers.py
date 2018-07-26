35bf25f071f6295f4a265314215e56477866ae56
Smedha245-unclejoy <priti7608@gmail.com>
2018-07-26 16:18:33 +0530

    post edit

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,7 +8,7 @@ class PostImageSerializer(serializers.ModelSerializer):
         fields = ('image','post')
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='user.id')
+    author=serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     images = PostImageSerializer(source='postimage_set', many=True, read_only=True)
     #body = serializers.CharField(required=True)
     class Meta:
